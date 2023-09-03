c56f134bb9a836be417c51805cfbc3f9f2357186
Mary <omota4fidelity@gmail.com>
2023-09-03 15:57:16 +0100

    Practiced

--- src/components/ChangeItUp.js
+++ src/components/ChangeItUp.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 function ChangeItUp() {
-  function handleChange(event) {
-    console.log(`${event.target.name}: ${event.target.value}`);
+  function handleEvent(e) {
+    console.log(`${e.target.name}: ${e.target.value}`);
   }
 
   return (
@@ -10,11 +10,11 @@ function ChangeItUp() {
       <input
         type="text"
         name="search"
-        onChange={handleChange}
+        onChange={handleEvent}
         placeholder="Enter search term..."
       />
 
-      <select name="filter" onChange={handleChange}>
+      <select name="filter" onChange={handleEvent}>
         <option value="all">Select a filter...</option>
         <option value="completed">Completed</option>
         <option value="incomplete">Incomplete</option>
